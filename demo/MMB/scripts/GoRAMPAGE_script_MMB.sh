d813dec746b7903a1dc11f983690b5cf61f507f4
rtraborn <rtraborn@indiana.edu>
2017-08-07 14:55:55 -0400

    Commiting VB's changes

--- demo/MMB/scripts/GoRAMPAGE_script_MMB.sh
+++ demo/MMB/scripts/GoRAMPAGE_script_MMB.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-bindir=<path/to/GoRAMPAGE/scripts #please fill this out before you continue
-workdir=<path/to/GoRAMPAGE/demo//MMB> #please fill this out before you continue
-GENOME=${workdir}/genome
+GoRAMPAGEhome=<enter full path to GoRAMPAGE directory> ## please change before proceeding
+workdir=<enter full to your work directory> ## please change before proceeding
+nProc=4 ## please set the number of processors to be used here
+
+bindir=${GoRAMPAGEhome}/scripts
 outputDir=${workdir}/output
 reads_dir=${outputDir}/reads/clipped
-rRNA=${workdir}/additional_files/Dmel_rRNA.fasta
-nProc=4
+rRNA=${GoRAMPAGEhome}/demo/MMB/additional_files/Dmel_rRNA.fasta
+GENOME=${workdir}/genome
+
 
 echo "GoRAMPAGE job starting"
-${bindir}/GoRAMPAGE -r $reads_dir -R $rRNA -g $GENOME -p $nProc --outputdir $outputDir --startfromstep step3 --stopatstep step5 >& errScript
-
-echo "GoRAMPAGE job is complete!"
+echo " ... you can monitor progress in a separate window with 'tail -f err'"
+echo ""
+${bindir}/GoRAMPAGE -r $reads_dir -R $rRNA -g $GENOME -p $nProc --outputdir $outputDir --startfromstep step3 --stopatstep step5 >& err
